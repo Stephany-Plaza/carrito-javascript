2c607b6d3e1ef27da42521a3062c143c86f4d76f
Stephany Plaza <splaza410@gmail.com>
2022-02-10 22:47:43 -0400

    prueba con otar

--- carrito.js
+++ carrito.js
@@ -5,30 +5,16 @@ import { productos } from "./data.js";
 
 const carrito = new Carrito();
 
-const renderList = (idLista, lista) => {
-    const listaContainer = document.getElementById('contenedor-productos');
-
-    for (const item of lista) {
-        const articulo = document.createElement('article')
-        const itemList = document.createElement('div');
-        const titulo4 = document.createElement('h4');
-        const imgProducto = document.createElement('img');
-        titulo4.innerHTML = `${item.nombre} ${item.precio}$ `;
-       
-        imgProducto.className='producto img';
-       imgProducto.src= item.img;
-       articulo.className='producto';
-        itemList.className='descripcion';
-        itemList.appendChild(titulo4);
-
-        
-        listaContainer.appendChild(imgProducto);
-        listaContainer.appendChild(itemList);
-        
-
-        itemList.addEventListener('click', () => {
-            console.log(`${item.nombre}`);
-            carrito.createItem(item);
+function renderList(){
+    productos.forEach((item)=>{
+        $('#productos-lista').append(`
+           <li id=${item.id} class='producto-nombre'>${item.nombre}</li>
+        `)
+    })
+}
+        /*itemList.addEventListener('click', () => {
+            console.log(`//${item.nombre}`);
+           /* carrito.createItem(item);
         })
         itemList.onmouseover = () => {
             itemList.style.backgroundColor = 'coral'
@@ -45,10 +31,4 @@ const botonCompra = document.getElementById('boton-carrito-compra');
 botonCompra.addEventListener('click', () => {
     alert(`usted ha comprado ${JSON.stringify(carrito.findAllItems())}`);
 
-})
-
-
-
-
-
-
+})*/
